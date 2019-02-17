9f3e27b32d07b5f4605f8b9f45e6ca5a70de5a42
Michael Goberling <Michael.Goberling@gmail.com>
2019-02-17 14:01:30 -0600

    Initial commit

---
+++ learning-angular/my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+  serverId = 10;
+  serverStatus: string = "offline";
+
+  getServerStatus() 
+  {
+    return this.serverStatus;
+  }
+}
